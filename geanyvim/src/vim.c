bc65d097baded27281c89f120de6928f05f2ce07
Jiří Techet <techet@gmail.com>
2018-03-07 16:24:57 +0100

    tmp

--- geanyvim/src/vim.c
+++ geanyvim/src/vim.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (C) 2018 Jiri Techet <techet@gmail.com>
- * Copyright (C) 2012 Colomban Wendling <ban@herbesfolles.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -58,11 +57,19 @@ struct
 	GtkWidget *toggle_vi_item;
 	GtkWidget *perform_vi_item;
 
-	gboolean vi_mode;
+	/* caret style used by Geany we can revert to when disabling vi mode */
+	gint default_caret_style;
+
+	/* whether vi mode is enabled or disabled */
+	gboolean vi_mode; 
+	/* if vi mode is valid for a single command and will be disabled automatically
+	 * after performing it */
 	gboolean onetime_vi_mode;
+	/* if we are in the insert mode or command mode of vi */
+	gboolean insert_mode; 
 } plugin_data =
 {
-	NULL, NULL, NULL, NULL, NULL, TRUE, FALSE
+	NULL, NULL, NULL, NULL, NULL, -1, TRUE, FALSE, FALSE
 };
 
 
@@ -106,43 +113,6 @@ static void on_prompt_show(GtkWidget *widget, gpointer dummy)
 
 
 
-static void create_prompt(void)
-{
-	GtkWidget *frame;
-	GtkWidget *box;
-
-	plugin_data.prompt = g_object_new(GTK_TYPE_WINDOW,
-			"decorated", FALSE,
-			"default-width", 500,
-			//"default-height", 200,
-			"transient-for", geany_data->main_widgets->window,
-			"window-position", GTK_WIN_POS_CENTER_ON_PARENT,
-			"type-hint", GDK_WINDOW_TYPE_HINT_DIALOG,
-			"skip-taskbar-hint", TRUE,
-			"skip-pager-hint", TRUE,
-			NULL);
-	g_signal_connect(plugin_data.prompt, "focus-out-event", G_CALLBACK(gtk_widget_hide), NULL);
-	g_signal_connect(plugin_data.prompt, "show", G_CALLBACK(on_prompt_show), NULL);
-	g_signal_connect(plugin_data.prompt, "hide", G_CALLBACK(on_prompt_hide), NULL);
-	g_signal_connect(plugin_data.prompt, "key-press-event", G_CALLBACK(on_prompt_key_press_event), NULL);
-
-	frame = gtk_frame_new(NULL);
-	gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_IN);
-	gtk_container_add(GTK_CONTAINER(plugin_data.prompt), frame);
-  
-	box = gtk_vbox_new(FALSE, 0);
-	gtk_container_add(GTK_CONTAINER(frame), box);
-
-	plugin_data.entry = gtk_entry_new();
-	gtk_box_pack_start(GTK_BOX(box), plugin_data.entry, FALSE, TRUE, 0);
-
-	/* connect entry signals after the view is created as they use it */
-	g_signal_connect(plugin_data.entry, "notify::text", G_CALLBACK(on_entry_text_notify), NULL);
-	g_signal_connect(plugin_data.entry, "activate", G_CALLBACK(on_entry_activate), NULL);
-
-	gtk_widget_show_all(frame);
-}
-
 
 static gchar *get_config_filename (void)
 {
@@ -151,14 +121,14 @@ static gchar *get_config_filename (void)
 
 static void load_config(void)
 {
-	gchar *filename = get_config_filename ();
-	GKeyFile *kf = g_key_file_new ();
+	gchar *filename = get_config_filename();
+	GKeyFile *kf = g_key_file_new();
 
-	if (g_key_file_load_from_file (kf, filename, G_KEY_FILE_NONE, NULL))
-		plugin_data.vi_mode = g_key_file_get_boolean (kf, CONF_GROUP, CONF_VI_MODE, NULL);
+	if (g_key_file_load_from_file(kf, filename, G_KEY_FILE_NONE, NULL))
+		plugin_data.vi_mode = g_key_file_get_boolean(kf, CONF_GROUP, CONF_VI_MODE, NULL);
   
-	g_key_file_free (kf);
-	g_free (filename);
+	g_key_file_free(kf);
+	g_free(filename);
 }
 
 
@@ -183,27 +153,43 @@ static void save_config(void)
 }
 
 
-static void enable_vi_mode()
+static void prepare_vi_mode(GeanyDocument *doc)
 {
-	
-}
+	ScintillaObject *sci;
 
-static void disable_vi_mode()
-{
-	
-}
+	if (!doc)
+		return;
 
+	sci = doc->editor->sci;
 
+	if (plugin_data.default_caret_style == -1)
+		plugin_data.default_caret_style = scintilla_send_message(sci, SCI_GETCARETSTYLE, 0, 0);
 
-static gboolean on_toggle_vim_mode(GeanyKeyBinding *kb, guint key_id, gpointer data)
-{
-	plugin_data.vi_mode = !plugin_data.vi_mode;
-	plugin_data.onetime_vi_mode = FALSE;
 	if (plugin_data.vi_mode)
-		enable_vi_mode();
+	{
+		if (plugin_data.insert_mode)
+			scintilla_send_message(sci, SCI_SETCARETSTYLE, CARETSTYLE_LINE, 0);
+		else
+			scintilla_send_message(sci, SCI_SETCARETSTYLE, CARETSTYLE_BLOCK, 0);
+	}
 	else
-		disable_vi_mode();
+		scintilla_send_message(sci, SCI_SETCARETSTYLE, plugin_data.default_caret_style, 0);
+}
+
+
+static void on_toggle_vim_mode(void)
+{
+	plugin_data.vi_mode = gtk_check_menu_item_get_active(GTK_CHECK_MENU_ITEM(plugin_data.toggle_vi_item));
+	plugin_data.onetime_vi_mode = FALSE;
+	plugin_data.insert_mode = FALSE;
+	prepare_vi_mode(document_get_current());
 	save_config();
+}
+
+static gboolean on_toggle_vim_mode_kb(GeanyKeyBinding *kb, guint key_id, gpointer data)
+{
+	gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(plugin_data.toggle_vi_item),
+			!plugin_data.vi_mode);
 
 	return TRUE;
 }
@@ -214,8 +200,10 @@ static gboolean on_perform_vim_command(GeanyKeyBinding *kb, guint key_id, gpoint
 	{
 		plugin_data.onetime_vi_mode = TRUE;
 		plugin_data.vi_mode = TRUE;
-		enable_vi_mode();
 	}
+	plugin_data.insert_mode = FALSE;
+	prepare_vi_mode(document_get_current());
+
 	return TRUE;
 }
 
@@ -225,7 +213,10 @@ static gboolean on_key_press(GtkWidget *widget, GdkEventKey *event, gpointer use
 	{
 		if (event->keyval == GDK_KEY_colon || event->keyval == GDK_KEY_slash)
 		{
+			const gchar *val = event->keyval == GDK_KEY_colon ? ":" : "/";
 			gtk_widget_show(plugin_data.prompt);
+			gtk_entry_set_text(GTK_ENTRY(plugin_data.entry), val);
+			gtk_editable_set_position(GTK_EDITABLE(plugin_data.entry), 1);
 		}
 		return TRUE;
 	}
@@ -241,11 +232,20 @@ static void on_doc_open(G_GNUC_UNUSED GObject *obj, GeanyDocument *doc,
 
 	plugin_signal_connect(geany_plugin, G_OBJECT(doc->editor->sci), "key-press-event",
 			FALSE, G_CALLBACK(on_key_press), NULL);
+	prepare_vi_mode(doc);
 }
 
+static void on_doc_activate(G_GNUC_UNUSED GObject *obj, GeanyDocument *doc,
+		G_GNUC_UNUSED gpointer user_data)
+{
+	g_return_if_fail(doc != NULL);
+
+	prepare_vi_mode(doc);
+}
 
 PluginCallback plugin_callbacks[] = {
 	{"document-open", (GCallback) &on_doc_open, TRUE, NULL},
+	{"document-activate", (GCallback) &on_doc_activate, TRUE, NULL },
 	{NULL, NULL, FALSE, NULL}
 };
 
@@ -253,27 +253,25 @@ PluginCallback plugin_callbacks[] = {
 void plugin_init(GeanyData *data)
 {
 	GeanyKeyGroup *group;
+	GtkWidget *frame;
 	gsize i;
 
 	load_config();
 
-	foreach_document(i)
-		plugin_signal_connect(geany_plugin, G_OBJECT(documents[i]->editor->sci),
-				"key-press-event", FALSE, G_CALLBACK(on_key_press), NULL);
-
+	/* menu items and keybindings */
 	group = plugin_set_key_group(geany_plugin, "geanyvim", KB_COUNT, NULL);
 
 	plugin_data.separator_item = gtk_separator_menu_item_new();
 	gtk_container_add(GTK_CONTAINER(geany->main_widgets->tools_menu), plugin_data.separator_item);
 
 	plugin_data.toggle_vi_item = gtk_check_menu_item_new_with_mnemonic(_("Enable _Vim Mode"));
-	gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(plugin_data.toggle_vi_item),
-			plugin_data.vi_mode);
 	gtk_widget_show(plugin_data.toggle_vi_item);
 	gtk_container_add(GTK_CONTAINER(geany->main_widgets->tools_menu), plugin_data.toggle_vi_item);
 	g_signal_connect((gpointer) plugin_data.toggle_vi_item, "activate", G_CALLBACK(on_toggle_vim_mode), NULL);
+	gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(plugin_data.toggle_vi_item),
+			plugin_data.vi_mode);
 	keybindings_set_item_full(group, KB_TOGGLE_VIM_MODE, 0, 0, "toggle_vim",
-			_("Enable Vim Mode"), NULL, on_toggle_vim_mode, NULL, NULL);
+			_("Enable Vim Mode"), NULL, on_toggle_vim_mode_kb, NULL, NULL);
 
 	plugin_data.perform_vi_item = gtk_menu_item_new_with_mnemonic(_("_Perform Vim Command"));
 	gtk_widget_show(plugin_data.perform_vi_item);
@@ -282,12 +280,54 @@ void plugin_init(GeanyData *data)
 	keybindings_set_item_full(group, KB_PERFORM_VIM_COMMAND, 0, 0, "vim_command",
 			_("Perform Vim Command"), NULL, on_perform_vim_command, NULL, NULL);
 
-	create_prompt();
+	/* prompt */
+	plugin_data.prompt = g_object_new(GTK_TYPE_WINDOW,
+			"decorated", FALSE,
+			"default-width", 500,
+			//"default-height", 200,
+			"transient-for", geany_data->main_widgets->window,
+			"window-position", GTK_WIN_POS_CENTER_ON_PARENT,
+			"type-hint", GDK_WINDOW_TYPE_HINT_DIALOG,
+			"skip-taskbar-hint", TRUE,
+			"skip-pager-hint", TRUE,
+			NULL);
+	g_signal_connect(plugin_data.prompt, "focus-out-event", G_CALLBACK(gtk_widget_hide), NULL);
+	g_signal_connect(plugin_data.prompt, "show", G_CALLBACK(on_prompt_show), NULL);
+	g_signal_connect(plugin_data.prompt, "hide", G_CALLBACK(on_prompt_hide), NULL);
+	g_signal_connect(plugin_data.prompt, "key-press-event", G_CALLBACK(on_prompt_key_press_event), NULL);
+
+	frame = gtk_frame_new(NULL);
+	gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_IN);
+	gtk_container_add(GTK_CONTAINER(plugin_data.prompt), frame);
+  
+	plugin_data.entry = gtk_entry_new();
+	gtk_container_add(GTK_CONTAINER(frame), plugin_data.entry);
+
+	g_signal_connect(plugin_data.entry, "notify::text", G_CALLBACK(on_entry_text_notify), NULL);
+	g_signal_connect(plugin_data.entry, "activate", G_CALLBACK(on_entry_activate), NULL);
+
+	gtk_widget_show_all(frame);
+
+	/* final setup */
+	foreach_document(i)
+		plugin_signal_connect(geany_plugin, G_OBJECT(documents[i]->editor->sci),
+				"key-press-event", FALSE, G_CALLBACK(on_key_press), NULL);
+	prepare_vi_mode(document_get_current());
 }
 
 
 void plugin_cleanup(void)
 {
+	if (plugin_data.default_caret_style != -1)
+	{
+		gsize i;
+		foreach_document(i)
+		{
+			ScintillaObject *sci = documents[i]->editor->sci;
+			scintilla_send_message(sci, SCI_SETCARETSTYLE, plugin_data.default_caret_style, 0);
+		}
+	}
+
 	gtk_widget_destroy(plugin_data.prompt);
 	gtk_widget_destroy(plugin_data.separator_item);
 	gtk_widget_destroy(plugin_data.toggle_vi_item);
